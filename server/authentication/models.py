eb3ef38f22cce7713b7079ca1988992cb494b890
nikhil <nikhilmanocha99@gmail.com>
2022-02-17 14:26:43 +0530

    Login page pushe

--- server/authentication/models.py
+++ server/authentication/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+def register(models.Model):
+    fname = models.Charfield(max_length = 30)
+    lname = models.Charfield(max_length = 30)
+    email = models.Emailfield()
+    password = models.Passwordfield()
+    cpassword = models.Passwordfield()
+    pnum = models.IntegerField()
--- server/authentication/views.py
+++ server/authentication/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main(request):
+    return render(request, 'dashboard.html')
+    
+def remark(request):
+    return render(request, 'remark.html')
